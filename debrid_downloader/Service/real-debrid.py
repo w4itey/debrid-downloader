bc518899610df41540785b7196d761c2ca3ae210
Ken Schiano <kschiano3@gatech.edu>
2024-08-02 14:13:43 -0400

    updates

--- debrid_downloader/Service/real-debrid.py
+++ debrid_downloader/Service/real-debrid.py
@@ -1,6 +1,7 @@
 import requests
 import yaml
 from pprint import pprint
+import re
 
 
 class debrid:
@@ -8,13 +9,18 @@ class debrid:
     def __init__(self, config) -> None:
         self.url_base = "https://api.real-debrid.com/rest/1.0/"
         self.api_key = config["api_key"]
+        self.sonarr_watch_folder = config["sonarr_blackhole_dir"]
+        self.radarr_watch_folder = config["radarr_blackhole_dir"]
 
     def request(self, url, type, body=None):
         headers = {"Authorization": f"Bearer {self.api_key}"}
 
         if type == "GET":
             response = requests.get(url, headers=headers)
-            return response.json()
+
+        if type == "POST":
+            response = requests.post(url, headers=headers, data=body)
+        return response.json()
 
     def get_current_torrents(self):
         url = f"{self.url_base}/torrents"
@@ -23,11 +29,37 @@ class debrid:
 
         pprint(response)
 
-    def get_torrent_hash(self, filename):
-        pass
+    def get_torrent_hash(self, filename, isRadarr=True):
 
-    def get_instant_availability(self, hash):
+        if isRadarr == True:
+            magnet = f"{self.radarr_watch_folder}/{filename}"
+        elif isRadarr == False:
+            magnet = f"{self.sonarr_watch_folder}/{filename}"
+
+        with open(magnet) as t:
+            torrent = t.read()
+        p = re.search("btih:(.*)&dn", torrent)
+        magnet_hash = p.group(1)
+
+        compiled = {"hash": magnet_hash, "magnet": torrent}
+
+        return compiled
+
+    def get_instant_availability(self, hash=None):
         url = f"{self.url_base}/torrents/instantAvailability/{hash}"
+
+        response = self.request(url, type="GET")
+
+    def download_magnet(self, magnet):
+
+        url = f"{self.url_base}/torrents/addMagnet"
+
+        body = {"magnet": magnet["magnet"]}
+        response = self.request(url, type="POST", body=body)
+        url = response["uri"]
+        response = self.request(url, type="GET")
+
+        pprint(response)
 
 
 if __name__ == "__main__":
@@ -35,5 +67,11 @@ if __name__ == "__main__":
     with open("config.yml") as f:
         config = yaml.safe_load(f)
 
-    service = debrid(api_key=config)
-    service.get_current_torrents()
+    config["radarr_blackhole_dir"] = "tests/torrents"
+    config["sonarr_blackhole_dir"] = "test/torrents"
+    service = debrid(config)
+    torrent = service.get_torrent_hash(
+        "Dune.2021.2160p.BluRay.REMUX.HEVC.DTS-HD.MA.TrueHD.7.1.Atmos-FGT.magnet"
+    )
+
+    service.download_magnet(torrent)
